786b45ed9bbb397caf35b26bb463a10309ecaba3
Mina Farrokhnia <Mfarrokhnia@catalystone.com>
2020-11-05 10:38:50 +0100

    updated based on aws-version-3

--- main.tf
+++ main.tf
@@ -31,31 +31,46 @@ resource "aws_acm_certificate" "wildcard" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  zone_id = data.aws_route53_zone.main.id
-  name    = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_name
-  type    = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_type
-  ttl     = 60
+  # zone_id = data.aws_route53_zone.main.id
+  # name    = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_name
+  # type    = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_type
+  # ttl     = 60
 
-  records = [
-    tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_value,
-  ]
+  # records = [
+  #   tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_value,
+  # ]
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options: dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id = data.aws_route53_zone.main.id
 }
 
 resource "aws_acm_certificate_validation" "main" {
   count           = var.wait_for_validation == true ? 1 : 0
-  certificate_arn = aws_acm_certificate.main.arn
+  # certificate_arn = aws_acm_certificate.main.arn
 
-  validation_record_fqdns = [
-    aws_route53_record.cert_validation.fqdn,
-  ]
+  # validation_record_fqdns = [
+  #   aws_route53_record.cert_validation.fqdn,
+  # ]
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation: record.fqdn]
 }
 
 resource "aws_acm_certificate_validation" "wildcard" {
   count           = var.create_wildcard == true && var.wait_for_validation == true ? 1 : 0
   certificate_arn = aws_acm_certificate.wildcard[0].arn
 
-  validation_record_fqdns = [
-    aws_route53_record.cert_validation.fqdn,
-  ]
+  # validation_record_fqdns = [
+  #   aws_route53_record.cert_validation.fqdn,
+  # ]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation: record.fqdn]
 }
 
